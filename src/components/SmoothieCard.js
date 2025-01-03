92d1cff21d4300dcf413f585f9c359e67837f366
Ujval Patel <123365727+Ujvalp@users.noreply.github.com>
2025-01-04 02:10:55 +0530

    code update

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,36 +1,42 @@
-import { Link } from "react-router-dom"
-import supabase from "../config/supabaseClient"
+import { Link } from "react-router-dom";
+import supabase from "../config/supabaseClient";
 
-const SmoothieCard = ({ smoothie, onDelete }) => {
+const SmoothieCard = ({ smoothie, onDelete, sdetails }) => {
+  const handleDelete = async () => {
+    const { data, error } = await supabase
+      .from("smoothies")
+      .delete()
+      .eq("id", smoothie.id);
 
-    const handleDelete = async () => {
-        const { data, error } = await supabase
-        .from('smoothies')
-        .delete()
-        .eq('id', smoothie.id)
-
-        if(error){
-            console.log(error)
-        }
-        else {
-            console.log(data)
-            onDelete(smoothie.id)
-        }
-
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data);
+      onDelete(smoothie.id);
     }
-    return (
-        <div className="smoothie-card">
-            <h3>{smoothie.title}</h3>
-            <p>{smoothie.method}</p>
-            <div className="rating"> {smoothie.rating}</div>
-            <div className="button">
-                <Link to={'/' + smoothie.id}>
-                    <i className="material-icons">edit</i>
-                </Link>
-                <i className="material-icons" onClick={handleDelete}>delete</i>
-            </div>
-        </div>
-    )
-}
+  };
+  return (
+    <div className="smoothie-card">
+      <h3>{smoothie.title}</h3>
+      {/* <ul>
+        {sdetails.map((detail) =>
+          detail.method
+            .split("\n")
+            .map((line, index) => <li key={index}>{line}</li>)
+        )}
+      </ul> */}
+      <p>{smoothie.method}</p>
+      <div className="rating"> {smoothie.rating}</div>
+      <div className="button">
+        <Link to={"/" + smoothie.id}>
+          <i className="material-icons">edit</i>
+        </Link>
+        <i className="material-icons" onClick={handleDelete}>
+          delete
+        </i>
+      </div>
+    </div>
+  );
+};
 
-export default SmoothieCard
+export default SmoothieCard;
